352a4d8a06d9ecb64208324807c2916422886b47
Qnkacz <ufus09@gmail.com>
2021-04-01 16:06:54 +0200

    fixed WillPopScope

--- lib/LibGenBody.dart
+++ lib/LibGenBody.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:z_lib_app/API_Management.dart';
 import 'package:z_lib_app/Book.dart';
 import 'package:z_lib_app/ClassNames.dart';
@@ -11,6 +12,7 @@ class LibGenBody extends StatefulWidget {
 }
 
 class _LibGenBodyState extends State<LibGenBody> {
+  ScrollController scrollController;
   enterName(){
     setState(() {
       LibGen.PageNumber = 1;
@@ -38,62 +40,108 @@ class _LibGenBodyState extends State<LibGenBody> {
     }));
   }
   @override
+  Future<bool> exitDialoge() {
+    return showDialog(
+        context: context,
+        builder: (context)=>BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 3,sigmaY: 3),
+          child: new AlertDialog(
+            backgroundColor: Colors.blueGrey,
+            elevation: 0,
+            title: Text("You sure?"),
+            content: Text("You really wanna leave so soon?"),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(color: Color(0xffD9B7AB))),
+            actions: [
+              MaterialButton(onPressed: (){
+                SystemNavigator.pop();
+              },
+                child: Text("EXIT",style: TextStyle(color: Color(0xffD9B7AB)),),
+              ),
+              MaterialButton(onPressed: (){
+                Navigator.of(context).pop(false);
+              },
+                child: Text("CANCEL",style: TextStyle(color: Color(0xffD9B7AB))),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+  void initState() {
+    scrollController = new ScrollController()..addListener(_loadMore);
+    super.initState();
+  }
+  void _loadMore() {
+    //todo: review and do
+    // if (scrollController.position.pixels ==
+    //     scrollController.position.maxScrollExtent) {
+    //   Utilities.PageNumber++;
+    //   print(Utilities.PageNumber);
+    //   enterBookNameWithpage(Utilities.PageNumber);
+    // }
+  }
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: ListView.builder(
-          itemCount: LibGen.LibGenbookList.length,
-          itemBuilder: (context,index){
-            if (LibGen.LibGenbookList.length == 0) {
-              return Container();
-            }
-            if (index == LibGen.LibGenbookList.length) {
-              if(LibGen.LibGenbookList.length>=100){
-                return LinearProgressIndicator(
-                  backgroundColor: Colors.grey[900],
-                  valueColor:
-                  new AlwaysStoppedAnimation<Color>(Colors.grey),
-                );
-              }
-              else{
+    return WillPopScope(
+      onWillPop: (){
+        exitDialoge();
+        return Future.value(false);
+      },
+      child: Scaffold(
+        body: SafeArea(child: ListView.builder(
+            itemCount: LibGen.LibGenbookList.length,
+            itemBuilder: (context,index){
+              if (LibGen.LibGenbookList.length == 0) {
                 return Container();
               }
-            }
-            final item = LibGen.LibGenbookList[index];
-            return LibGenBookCard(
-              book: LibGen.LibGenbookList[index],
-              publisherSearch: (String val)=>searchPublisher(val),
-            );
-          })),
-      bottomSheet: Container(
-        color: Color(0xffd9b7ab),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                    flex: 8,
-                    child: Container(
-                        color: Color(0xff8c6f72),
-                        child: TextField(
-                            onEditingComplete: ()=>enterName(),//enterDOI(),
-                            textAlign: TextAlign.center,
-                            controller: LibGen.LibGenTextController,
-                            cursorColor: Colors.white70,
-                            style: TextStyle(color: Colors.white70,fontWeight: FontWeight.bold),
-                            decoration: InputDecoration(
-                              hintText: "Searched phrase goes here",
-                              hintStyle: TextStyle(color: Colors.white70,fontStyle: FontStyle.italic),
-                              border: InputBorder.none,
-                            ))
-                    )
-                ),
-                Expanded(
-                    flex: 2,
-                    child: MaterialButton(onPressed: ()=>enterName(),child: Icon(Icons.search))),//enterDOI(),))
-              ],
-            ),
-          ],
+              if (index == LibGen.LibGenbookList.length) {
+                if(LibGen.LibGenbookList.length>=100){
+                  return LinearProgressIndicator(
+                    backgroundColor: Colors.grey[900],
+                    valueColor:
+                    new AlwaysStoppedAnimation<Color>(Colors.grey),
+                  );
+                }
+                else{
+                  return Container();
+                }
+              }
+              final item = LibGen.LibGenbookList[index];
+              return LibGenBookCard(
+                book: LibGen.LibGenbookList[index],
+                publisherSearch: (String val)=>searchPublisher(val),
+              );
+            })),
+        bottomSheet: Container(
+          color: Color(0xffd9b7ab),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      flex: 8,
+                      child: Container(
+                          color: Color(0xff8c6f72),
+                          child: TextField(
+                              onEditingComplete: ()=>enterName(),//enterDOI(),
+                              textAlign: TextAlign.center,
+                              controller: LibGen.LibGenTextController,
+                              cursorColor: Colors.white70,
+                              style: TextStyle(color: Colors.white70,fontWeight: FontWeight.bold),
+                              decoration: InputDecoration(
+                                hintText: "Searched phrase goes here",
+                                hintStyle: TextStyle(color: Colors.white70,fontStyle: FontStyle.italic),
+                                border: InputBorder.none,
+                              ))
+                      )
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: MaterialButton(onPressed: ()=>enterName(),child: Icon(Icons.search))),//enterDOI(),))
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/SciHubBody.dart
+++ lib/SciHubBody.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:z_lib_app/API_Management.dart';
 import 'package:z_lib_app/Book.dart';
 import 'package:z_lib_app/ClassNames.dart';
@@ -11,7 +12,7 @@ class SciHubBody extends StatefulWidget {
 }
 
 class _SciHubBodyState extends State<SciHubBody> {
-
+  ScrollController scrollController;
   enterDOI(){
     setState(() {
       Utilities.PageNumber = 1;
@@ -26,66 +27,98 @@ class _SciHubBodyState extends State<SciHubBody> {
     })
     );
   }
-
+  Future<bool> exitDialoge() {
+    return showDialog(
+        context: context,
+        builder: (context)=>BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 3,sigmaY: 3),
+          child: new AlertDialog(
+            backgroundColor: Colors.blueGrey,
+            elevation: 0,
+            title: Text("You sure?"),
+            content: Text("You really wanna leave so soon?"),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(color: Color(0xffD9B7AB))),
+            actions: [
+              MaterialButton(onPressed: (){
+                SystemNavigator.pop();
+              },
+                child: Text("EXIT",style: TextStyle(color: Color(0xffD9B7AB)),),
+              ),
+              MaterialButton(onPressed: (){
+                Navigator.of(context).pop(false);
+              },
+                child: Text("CANCEL",style: TextStyle(color: Color(0xffD9B7AB))),
+              ),
+            ],
+          ),
+        )
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: ListView.builder(
-              itemCount: Utilities.SciHubAritcleList.length,
-              itemBuilder: (context,index){
-                final item = Utilities.SciHubAritcleList[index];
-                return SciHubArticle(articleInfo: item);
-          })
-      ),
-      bottomSheet: Container(
-        color: Color(0xffd9b7ab),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: (){
-                    return showDialog(
-                      context: context,
-                      builder: (context)=>BackdropFilter(filter: ImageFilter.blur(sigmaX: 3,sigmaY: 3),child: AlertDialog(
-                        backgroundColor: Colors.blueGrey,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(color: Color(0xffD9B7AB))),
-                        title: Text("Instructions"),
-                        content: Text("To use this site, you have to copy the URL, PMID or DOI link for the article you want to receive from Sci-Hub"),
-                      actions: [
-                        MaterialButton(onPressed: ()=>Navigator.of(context).pop(false),child: Text("OK",style: TextStyle(fontWeight: FontWeight.bold,color:Color(0xffD9B7AB),fontSize: 15))),
-                        MaterialButton(onPressed: (){Navigator.of(context).pop(false);API_Manager.LaunchInBrowser("https://en.wikipedia.org/wiki/Digital_object_identifier");},child: Text("About DOI",style: TextStyle(fontWeight: FontWeight.bold,color:Color(0xffD9B7AB),fontSize: 15))),
-                      ],
-                      ),)
-                    );
-                  },
-                  icon: Icon(Icons.info_outline,),
-                ),
-                Expanded(
-                    flex: 8,
-                    child: Container(
-                    color: Color(0xff8c6f72),
-                    child: TextField(
-                      onEditingComplete: ()=>enterDOI(),
-                        textAlign: TextAlign.center,
-                        controller: Utilities.SciHubTextController,
-                        cursorColor: Colors.white70,
-                        style: TextStyle(color: Colors.white70,fontWeight: FontWeight.bold),
-                        decoration: InputDecoration(
-                          hintText: "enter URL, PMID / DOI",
-                          hintStyle: TextStyle(color: Colors.white70,fontStyle: FontStyle.italic),
-                          border: InputBorder.none,
-                        ))
-                  )
-                ),
-                Expanded(
-                    flex: 2,
-                    child: MaterialButton(onPressed: ()=>enterDOI(),child: Icon(Icons.search),))
-              ],
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: (){
+        exitDialoge();
+        return Future.value(false);
+      },
+      child: Scaffold(
+        body: SafeArea(
+            child: ListView.builder(
+                itemCount: Utilities.SciHubAritcleList.length,
+                itemBuilder: (context,index){
+                  final item = Utilities.SciHubAritcleList[index];
+                  return SciHubArticle(articleInfo: item);
+            })
+        ),
+        bottomSheet: Container(
+          color: Color(0xffd9b7ab),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: (){
+                      return showDialog(
+                        context: context,
+                        builder: (context)=>BackdropFilter(filter: ImageFilter.blur(sigmaX: 3,sigmaY: 3),child: AlertDialog(
+                          backgroundColor: Colors.blueGrey,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(color: Color(0xffD9B7AB))),
+                          title: Text("Instructions"),
+                          content: Text("To use this site, you have to copy the URL, PMID or DOI link for the article you want to receive from Sci-Hub"),
+                        actions: [
+                          MaterialButton(onPressed: ()=>Navigator.of(context).pop(false),child: Text("OK",style: TextStyle(fontWeight: FontWeight.bold,color:Color(0xffD9B7AB),fontSize: 15))),
+                          MaterialButton(onPressed: (){Navigator.of(context).pop(false);API_Manager.LaunchInBrowser("https://en.wikipedia.org/wiki/Digital_object_identifier");},child: Text("About DOI",style: TextStyle(fontWeight: FontWeight.bold,color:Color(0xffD9B7AB),fontSize: 15))),
+                        ],
+                        ),)
+                      );
+                    },
+                    icon: Icon(Icons.info_outline,),
+                  ),
+                  Expanded(
+                      flex: 8,
+                      child: Container(
+                      color: Color(0xff8c6f72),
+                      child: TextField(
+                        onEditingComplete: ()=>enterDOI(),
+                          textAlign: TextAlign.center,
+                          controller: Utilities.SciHubTextController,
+                          cursorColor: Colors.white70,
+                          style: TextStyle(color: Colors.white70,fontWeight: FontWeight.bold),
+                          decoration: InputDecoration(
+                            hintText: "enter URL, PMID / DOI",
+                            hintStyle: TextStyle(color: Colors.white70,fontStyle: FontStyle.italic),
+                            border: InputBorder.none,
+                          ))
+                    )
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: MaterialButton(onPressed: ()=>enterDOI(),child: Icon(Icons.search),))
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
